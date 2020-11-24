c23ad47d150960e0a0415ce8f39ddb3fc92eabb5
Rasmus Deckmann <rasm732d@edu.easj.dk>
2020-11-24 12:35:58 +0100

    Added Controllers

---
+++ iFridgeAPI/Controllers/WaresController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FridgeModels;
+using iFridgeAPI.Data;
+
+namespace iFridgeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaresController : ControllerBase
+    {
+        private readonly ContextWare _context;
+
+        public WaresController(ContextWare context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Wares
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ware>>> GetWares()
+        {
+            return await _context.Wares.ToListAsync();
+        }
+
+        // GET: api/Wares/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ware>> GetWare(int id)
+        {
+            var ware = await _context.Wares.FindAsync(id);
+
+            if (ware == null)
+            {
+                return NotFound();
+            }
+
+            return ware;
+        }
+
+        // PUT: api/Wares/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWare(int id, Ware ware)
+        {
+            if (id != ware.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ware).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WareExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Wares
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Ware>> PostWare(Ware ware)
+        {
+            _context.Wares.Add(ware);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWare", new { id = ware.Id }, ware);
+        }
+
+        // DELETE: api/Wares/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Ware>> DeleteWare(int id)
+        {
+            var ware = await _context.Wares.FindAsync(id);
+            if (ware == null)
+            {
+                return NotFound();
+            }
+
+            _context.Wares.Remove(ware);
+            await _context.SaveChangesAsync();
+
+            return ware;
+        }
+
+        private bool WareExists(int id)
+        {
+            return _context.Wares.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ iFridgeAPI/Controllers/WaretypesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FridgeModels;
+using iFridgeAPI.Data;
+
+namespace iFridgeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaretypesController : ControllerBase
+    {
+        private readonly ContextWaretype _context;
+
+        public WaretypesController(ContextWaretype context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Waretypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Waretype>>> GetWaretypes()
+        {
+            return await _context.Waretypes.ToListAsync();
+        }
+
+        // GET: api/Waretypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Waretype>> GetWaretype(int id)
+        {
+            var waretype = await _context.Waretypes.FindAsync(id);
+
+            if (waretype == null)
+            {
+                return NotFound();
+            }
+
+            return waretype;
+        }
+
+        // PUT: api/Waretypes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWaretype(int id, Waretype waretype)
+        {
+            if (id != waretype.Barcode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(waretype).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WaretypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Waretypes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Waretype>> PostWaretype(Waretype waretype)
+        {
+            _context.Waretypes.Add(waretype);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWaretype", new { id = waretype.Barcode }, waretype);
+        }
+
+        // DELETE: api/Waretypes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Waretype>> DeleteWaretype(int id)
+        {
+            var waretype = await _context.Waretypes.FindAsync(id);
+            if (waretype == null)
+            {
+                return NotFound();
+            }
+
+            _context.Waretypes.Remove(waretype);
+            await _context.SaveChangesAsync();
+
+            return waretype;
+        }
+
+        private bool WaretypeExists(int id)
+        {
+            return _context.Waretypes.Any(e => e.Barcode == id);
+        }
+    }
+}
