e7b82d6e2a7e23631aaa767a839336b08d9beccd
Rasmus Deckmann <rasm732d@edu.easj.dk>
2020-11-26 13:17:44 +0100

    Har sat selenium op til Fjern objekt

---
+++ Sel Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Sel_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
