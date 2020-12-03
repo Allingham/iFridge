9a72f733c977b3a91a601e80348d2f30f6766d5e
Maja Seehusen Bloch <maja8202@edu.easj.dk>
2020-12-03 11:11:12 +0100

    Selenium test på dropdown

--- Sel Test/UnitTest1.cs
+++ Sel Test/UnitTest1.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Sel_Test
 {
@@ -135,6 +137,35 @@ namespace Sel_Test
 
         }
 
+        public void DropDownSubCategoryTest()
+        {
+            //Her får vi fat på Dropdown elementet (gennem id fra HTML side)
+            IWebElement selectSubCategoryDropdown = driver.FindElement(By.Id("subCategoryDropdown"));
+
+            //her får vi fat i vores dropdown muligheder
+            SelectElement selectSubCategory = new SelectElement(selectSubCategoryDropdown);
+
+            //her får jeg fat i alle muligheder og smider dem i en liste
+            IList<IWebElement> elements = selectSubCategory.Options;
+
+            //her tæller jeg hvor mange muligheder subCategory der i i listen
+            int elementsSize = elements.Count;
+
+            //her får jeg fat i vores table med alle subcategory elementer fra vores DB
+            var objektListEnd = driver.FindElements(By.Id("SubCategoryList"));
+            //Her tæller jeg listen
+            var subCategoryCount = objektListEnd.Count;
+
+            //selve testen der sammenligner de 2 antal fra table og fra category options
+            Assert.AreEqual(elementsSize, subCategoryCount);
+
+
+            //Her får jeg fat på et det første element inden i cubCategoryListen
+            var cell = driver.FindElements(By.Id("subCategoryColumn")).First();
+            //her tjekker jeg at valuen af første element er hvad jeg forventer
+            Assert.AreEqual("Mælk", cell.Text);
+
+        }
 
 
         [TestCleanup]
